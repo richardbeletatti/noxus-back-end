2da295b8d6c1fba322128d24b4434918939eedf2
Richard Beletatti <richard.beletattil@gmail.com>
2025-05-13 16:52:30 -0300

    Created the CRM project

---
+++ src/test/java/com/alemcrm/AlemCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alemcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlemCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
